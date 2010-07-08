96a5c5b0cc4deb578865eb457b63578a7b27cb0d
Spencer Williams <s@spencerenglish.com>
2010-07-08 08:19:00 +0000

    slug validation, now the lib file applies to more than one model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ActiveRecord::Base
 
   validates_presence_of :item_category, :name, :catalog_number
   validates_numericality_of :catalog_number
-  validates_uniqueness_of :slug
+  validates :catalog_number, :unique_slug => true
   
   def slug
     "#{:item_category.slug}-#{(:catalog_number.to_s.length < 3)? ("0"):("")}#{(:catalog_number.to_s.length < 2)? ("0"):("")}#{:catalog_number}"
--- lib/unique_slug_validator.rb
+++ lib/unique_slug_validator.rb
@@ -1,6 +1,6 @@
 class UniqueSlugValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    if !object.slug.nil? and ItemCategory.all.map {|ic| ic.slug }.compact.include? value
+    if !object.slug.nil? and object.class.all.map {|ic| ic.slug }.compact.include? object.slug
       object.errors[attribute] << (options[:message] || "makes for a nonunique slug")
     end
   end
