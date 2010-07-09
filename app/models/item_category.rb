30eb9f42dd9d60b55128dc60c7e40b9b1602db94
Spencer Williams <s@spencerenglish.com>
2010-07-09 23:46:10 +0000

    better validation for item categories, and we no longer need the object exists validator

--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -3,7 +3,7 @@ class ItemCategory < ActiveRecord::Base
   belongs_to :parent_category, :class_name => "ItemCategory" # foreign key parent_category_id
   has_many :items
   
-  validates :parent_category_id, :object_exists => {:allow_nil => true, :allow_blank => true}
+  validate :parent_category_exists
   validates :prefix, :presence => true, :numericality => true, :unique_slug => {:allow_nil => true, :allow_blank => true}
   
   # this scope selects categories that don't have parents. they are the top-level parents
@@ -49,4 +49,16 @@ class ItemCategory < ActiveRecord::Base
     return nil if parent_category.nil?
     "#{parent_category.prefix.to_s}%02d" % prefix.to_s
   end
+  
+protected
+
+  def parent_category_exists
+    return if parent_category_id.nil? or parent_category_id.blank?
+    begin
+      ItemCategory.find(parent_category_id)
+    rescue RecordNotFound
+      errors[parent_category_id] << "does not correspond to a valid Item Category"
+    end
+  end
+  
 end
