caa31acd6f1cd78d31b963c5187bc3d7a8987d10
Spencer Williams <s@spencerenglish.com>
2010-07-09 01:32:30 +0000

    validation and slug

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ActiveRecord::Base
   belongs_to :item_category # foreign key item_category_id
 
-  validates_presence_of :item_category, :name, :catalog_number
+  validates_presence_of :name, :catalog_number
   validates_numericality_of :catalog_number
+  validates :item_category_id, :object_exists => true
   validates :catalog_number, :unique_slug => true
   
   def slug
-    "#{:item_category.slug}-#{(:catalog_number.to_s.length < 3)? ("0"):("")}#{(:catalog_number.to_s.length < 2)? ("0"):("")}#{:catalog_number}"
+    return nil if item_category_id.nil?
+    "#{item_category.slug}-%03d" % catalog_number
   end
 end
