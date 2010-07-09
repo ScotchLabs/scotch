2db1dbd64b7105212542d17cea1bdbe8cad59632
Spencer Williams <s@spencerenglish.com>
2010-07-09 23:28:22 +0000

    better sorting for ItemCategories\!

--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -9,9 +9,35 @@ class ItemCategory < ActiveRecord::Base
   # this scope selects categories that don't have parents. they are the top-level parents
   scope :parent_categories, where(:parent_category_id => nil).order("prefix ASC")
   
-  #TODO reimplement this as a <=>(other) method
-  def self.sorted
-    self.parent_categories.map {|ic| [ic,ic.item_subcategories.sort {|x, y| x.slug<=>y.slug}]}.flatten
+  def <=>(other)
+    # compare two top-level
+    if slug.nil? and other.slug.nil?
+      return prefix <=> other.prefix
+      
+    # compare this top-level and other second-level
+    elsif slug.nil?
+      # if other is inside this, it's below this
+      return -1 if other.parent_category_id == id
+      
+      # else compare top-levels
+      return prefix <=> other.parent_category.prefix unless other.parent_category_id == id
+      
+    # compare this second-level and other top-level
+    elsif other.slug.nil?
+      # if this is inside other, it's below other
+      return 1 if parent_category_id == other.id
+      
+      # else compare top-levels
+      return parent_category.prefix <=> other.prefix unless parent_category_id == other.id
+      
+    # compare two second-level
+    else
+      # if in the same top-level compare slugs
+      return slug <=> other.slug if parent_category_id == other.parent_category_id
+      
+      # else compare parents
+      return parent_category.prefix <=> other.parent_category.prefix unless  parent_category_id == other.parent_category_id
+    end
   end
   
   # useful for creating selects with optgroups of the top-level categories and options of the second-level categories
