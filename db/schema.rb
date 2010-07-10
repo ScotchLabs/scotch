1ce143bfa2b93d99f0a2880aff3341630fd32e53
Spencer Williams <s@spencerenglish.com>
2010-07-10 18:07:22 +0000

    seeding ItemCategories from current SotW. there's also a migration with the seeds for your seeding convenience. seedseedseed.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100701021138) do
+ActiveRecord::Schema.define(:version => 20100710175409) do
 
   create_table "checkout_events", :force => true do |t|
     t.string   "event"
