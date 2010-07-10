44505641f4bf7063c6dfc2a47e2e341f8af7471f
Spencer Williams <s@spencerenglish.com>
2010-07-10 18:19:44 +0000

    item sorting

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,13 @@ class Item < ActiveRecord::Base
   validates_presence_of :item_category
   validates :catalog_number, :presence => true, :numericality => true, :unique_slug => {:allow_nil => true, :allow_blank => true}
   
+  def <=>(other)
+    slug <=> other.slug
+  end
+  
   def slug
     return nil if item_category_id.nil?
+    return nil unless item_category.respond_to? "slug"
     "#{item_category.slug}-%03d" % catalog_number
   end
 end
