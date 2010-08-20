b32ffc9ff591ccb073917fb6c5a7d05d6c613b12
Anthony Chivetta <anthony@chivetta.org>
2010-08-20 01:14:04 +0000

    increase recent users window to two weeks, order by most recent

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@ class User < ActiveRecord::Base
   DEFAULT_PERMISSIONS = %w(createGroup)
   HOME_COLLEGES = %w(SCS H&SS CIT CFA MCS TSB SHS BXA)
 
-  scope :recent, where(["current_sign_in_at > ?", 2.hour.ago])
+  scope :recent, where(["current_sign_in_at > ?", 2.weeks.ago]).order("current_sign_in_at DESC")
 
 ####################
 # OBJECT OVERRIDES #
