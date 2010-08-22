a99bb7d51b3acaac6c2ae06065b7a31f1b7d5ef8
Anthony Chivetta <anthony@chivetta.org>
2010-08-22 18:29:19 +0000

    login fix

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,11 +4,11 @@ class Users::SessionsController < Devise::SessionsController
   def lookup_email
     unless params.nil? or params[:user].nil?
       if params[:user].has_key? :andrew_id
-        u = User.find_by_andrew_id(params[:user][:andrew_id])
-        if u.nil?
-          params[:user][:email] = "#{params[:user][:andrew_id]}@andrew.cmu.edu"
+        if params[:user][:andrew_id].include? "@"
+          params[:user][:email] = params[:user][:andrew_id]
         else
-          params[:user][:email] = u.email
+          u = User.find_by_andrew_id(params[:user][:andrew_id])
+          params[:user][:email] = u.nil? ? "#{params[:user][:andrew_id]}@andrew.cmu.edu" : u.email
         end
       end
     end
