6c929cd88b0b40f4f63682cbb2fa8e7b9ce85455
Anthony Chivetta <anthony@chivetta.org>
2010-09-11 04:59:46 +0000

    fix hair//makeup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100911044422) do
+ActiveRecord::Schema.define(:version => 20100911045724) do
 
   create_table "checkouts", :force => true do |t|
     t.integer  "group_id"
