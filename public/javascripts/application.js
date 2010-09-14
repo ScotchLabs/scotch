6cd55c14254af2aa7c829a85033de179c28d1e9b
Spencer Williams <s@spencerenglish.com>
2010-09-14 04:03:59 +0000

    dynamic sizing of the nav items

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,9 @@
 $(function(){
   $('.placeholder-shim').placeholder();
+  
+  numnav = $('#nav ul li').size();
+  $('#nav ul li').css('width',900/numnav-21);
+  $('#nav ul li a').css('width',900/numnav-21);
 })
 
 $(document).ready(function(){
