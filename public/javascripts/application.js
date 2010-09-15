f54ee8759280c14389e2a183d1f2bef753545681
Spencer Williams <s@spencerenglish.com>
2010-09-15 14:30:27 +0000

    broken feeback form ajax submission

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,35 @@
 $(function(){
+  // apply the placeholder shim for the rich and prosperous elements.
   $('.placeholder-shim').placeholder();
   
+  // dynamically size the navitems: because some people have more than others
   numnav = $('#nav ul li').size();
   $('#nav ul li').css('width',900/numnav-21);
   $('#nav ul li a').css('width',900/numnav-21);
-})
-
-$(document).ready(function(){
+  
+  // autocomplete stuff
   $('#user_identifier').autocomplete('/users.js');
   $('.user_identifier').autocomplete('/users.js');
+  
+  // hijack the Colorboxed feedback form
+  //hijackSubmitFeedbackLink();
 })
+
+// nutrum.com/weblog/unobtrusive-ajax-with-jquery-and-rails
+function hijackSubmitFeedbackLink() {
+  $('#feedback_form').submit(function() {
+    $(this).ajaxSubmit({
+      target: '#',
+      clearForm: true,
+      success: feedbackSuccess,
+      error: feedbackError
+    })
+    return false
+  })
+}
+function feedbackSuccess() {
+  console.log('feedback success!')
+}
+function feedbackError() {
+  console.log('feedback error!')
+}
