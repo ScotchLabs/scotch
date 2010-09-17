660d82ffe282ab224a173c9463fac5d3ee650e5f
Anthony Chivetta <anthony@chivetta.org>
2010-09-17 07:06:34 +0000

    links on calendar to the events

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,13 @@
 module DashboardHelper
+  def event_to_json(event)
+		# WTF Ruby is broken.
+		# string.gsub("'", "\\'") duplicates all following non-whitespace
+		def f(string) 
+      string.gsub("'") { |c| "\\'" } 
+    end
+    return "{title : '[#{f event.group.short_name}] #{f event.title}', 
+      start : '#{event.start_time.strftime("%Y-%m-%d")}', 
+      end : '#{event.end_time.strftime("%Y-%m-%d")}',
+      url : '#{event_url(event)}'}"
+  end
 end
