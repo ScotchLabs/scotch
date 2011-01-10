756490c99e0af6aba9dbef587646734698bf7472
Anthony Chivetta <anthony@chivetta.org>
2011-01-10 05:03:53 +0000

    better comments on the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# The current database dump consists of only Rocky and users who have logged
+# in more than once since the launch of the site.  It has all images removed
+# from users and groups and all email addresses have .blackhole.snstheatre.org
+# appended so that there aren't any email from development environment
+# mistakes.  All developers are encouraged to `rake db:reset' to use this new
+# data.  In the future, perhaps some of these sanitation actions should occur
+# on load time.
 
 SerializationHelper::Base.new(YamlDb::Helper).load "db/data.yml.gz"
 
