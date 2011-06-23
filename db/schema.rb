1877a6852121eda20b5c4ae0f4883ada02975983
Spencer Williams <s@spencerenglish.com>
2011-06-23 20:11:26 +0000

    because autocomplete inside a colorbox is broken

--- db/schema.rb
+++ db/schema.rb
@@ -199,7 +199,7 @@ ActiveRecord::Schema.define(:version => 20110622055242) do
   create_table "users", :force => true do |t|
     t.string   "email",                                :default => "",   :null => false
     t.string   "encrypted_password",    :limit => 128, :default => "",   :null => false
-    t.string   "password_salt"
+    t.string   "password_salt",                        :default => "",   :null => false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
