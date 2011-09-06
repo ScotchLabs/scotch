08e4dd8040bd9b34937bc309b262ac67315296d5
Spencer Williams <s@spencerenglish.com>
2011-09-06 02:49:03 +0000

    a little bit of restructuring, styling

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -5,10 +5,9 @@ module EventsHelper
 
     # keep going until we are out of events
     while (all_events.length > 0) do
-      # slide all elements off of the front of all_events with the same title
-      # and adjacent start/end times
+      # slide all elements off of the front of all_events that are repeat_children (or siblings) of the first
       events = [all_events.shift]
-      while (all_events.length > 0 && all_events[0].title == events[0].title && all_events[0].start_time == events[-1].end_time) do
+      while (all_events.length > 0 and all_events[0].repeat_id != nil and (events[0].id == all_events[0].repeat_id or events[0].repeat_id == all_events[0].repeat_id) ) do
         events.push all_events.shift
       end
 
