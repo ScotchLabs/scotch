8a3f2fad43c8c82f6e9444f966f4fcf2c2c9d757
Doci Mou <dmou@mikeyini.res.cmu.edu>
2012-03-17 12:08:40 -0400

    implemented elections page

--- app/models/election.rb
+++ app/models/election.rb
@@ -12,7 +12,10 @@
 class Election < ActiveRecord::Base
   
   validates_presence_of :name, :position, :platform
-  
+  POSITIONS = ["President", "Vice President", "Artistic Director", "Managing Director", "Technical Coordinator", "Secretary", "Public Relations Coordinator", "General Membership Coordinator", "Rights & Reservations Representative"]
+
+  #belongs_to :id
+
   define_index do
     indexes :name
     indexes :position
