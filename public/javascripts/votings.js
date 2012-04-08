349e0b6f9220c1d7024cb506011611bd75cbba6f
spilliams <s@spencerenglish.com>
2012-04-07 23:32:04 -0700

    stubs out more votings views

---
+++ public/javascripts/votings.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("a.showhide").click(function(){
+        $(this).parent().parent().parent().find('.collapsible').toggle('fast');
+        $(this).find('.showhide').toggle();
+    })
+})
