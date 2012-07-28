1fc1d068753406750d4fcf3e54354387152633ba
hmind <hmind2005@gmail.com>
2012-07-28 00:05:54 -0400

    Moved memory load to receiver script

--- app/workers/message_receive_worker.rb
+++ app/workers/message_receive_worker.rb
@@ -1,22 +1,7 @@
 class MessageReceiveWorker
   include Sidekiq::Worker
   
-  def perform(raw_email)
-    email = Mail.read_from_string(raw_email)
-    to = email.to.first
-    from = email.from.first
-    
-    message = ""
-    
-    if email.multipart?
-      part = email.parts.select { |p| p.content_type =~ /text\/plain/ }.first rescue nil
-      unless part.nil?
-        message = part.body.decoded
-      end
-    else
-      message = part.body.decoded
-    end
-    
+  def perform(to, from, message)
     user_id = to.scan(/-(\d+)@/).first.first
     thread_id = to.scan(/\+(\d+)-/).first.first
     
@@ -24,10 +9,10 @@ class MessageReceiveWorker
     thread = MessageThread.find(thread_id)
     
     #Experimental method of removing quoted reply text
-    message = message.scan(/(.*)\n.*#{Regexp.escape(to)}/m).first.first
+    filtered_message = message.scan(/(.*)\n.*#{Regexp.escape(to)}/m).first.first
     
     #TODO: Implement a check for whether they are allowed to post in this thread
-    thread.messages.create(user_id: user.id, text: message, priority: 'none')
+    thread.messages.create(user_id: user.id, text: filtered_message, priority: 'none')
     
     #TODO: Push to users via Faye
   end
--- message_receiver.rb
+++ message_receiver.rb
@@ -5,7 +5,22 @@ require File.expand_path('./app/workers/message_receive_worker.rb')
 
 class MessageReceive
   def initialize(raw_email)
-    Sidekiq::Client.enqueue(MessageReceiveWorker, raw_email)
+    email = Mail.read_from_string(raw_email)
+    to = email.to.first
+    from = email.from.first
+    
+    message = ""
+    
+    if email.multipart?
+      part = email.parts.select { |p| p.content_type =~ /text\/plain/ }.first rescue nil
+      unless part.nil?
+        message = part.body.decoded
+      end
+    else
+      message = part.body.decoded
+    end
+    
+    Sidekiq::Client.enqueue(MessageReceiveWorker, to, from, message)
   end
 end
 
