462cecb1270308e917743b36bfe5ae091f95c6f1
hmind <hmind2005@gmail.com>
2012-07-28 16:59:45 -0400

    Completed dashboard and body styling

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,6 +8,9 @@ class DashboardController < ApplicationController
       flash[:notice] = "Sorry, but we need you to fill out some profile information before continuing."
       redirect_to edit_user_path(current_user)
     end
+    
+    @threads = MessageThread.visible(current_user)
+    @events = current_user.attended_events
   end
 
   def calendar
