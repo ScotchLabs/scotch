18618410670cdff733995c45ccecb76b78f68fad
hmind <hmind2005@gmail.com>
2012-07-29 02:20:49 -0400

    Fixing OpenSSL error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,8 @@ Scotch::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   
-  config.action_mailer.default_url_options = { :host => 'snstheatre.org', :openssl_verify_mode => 'none' }
+  config.action_mailer.default_url_options = { :host => 'snstheatre.org' }
+  config.action_mailer.smtp_settings[:enable_starttls_auto] = false
 
   # Enable threaded mode
   # config.threadsafe!
