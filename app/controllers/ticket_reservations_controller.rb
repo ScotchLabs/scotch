c6cd15a7bbc22a29d9d7bbaf511acfc1d832c258
hmind <hmind2005@gmail.com>
2013-04-14 15:09:51 -0400

    work on reservation system

--- app/controllers/ticket_reservations_controller.rb
+++ app/controllers/ticket_reservations_controller.rb
@@ -32,17 +32,17 @@ class TicketReservationsController < ApplicationController
       return render action: 'new', error: 'No Performance Specified!'
     end
 
-    if @event.available_tickets < @reservation.amount
-      if !params[:waitlist]
-        return render action: 'waitlist'
-      elsif params[:waitlist] == 'all'
-        @reservation.waitlist_amount = @reservation.amount
-        @reservation.amount = 0
-      else
-        @reservation.waitlist_amount = @reservation.amount - @event.available_tickets
-        @reservation.amount = @event.available_tickets
-      end
-    end
+    # if @event.available_tickets < @reservation.amount
+    #   if !params[:waitlist]
+    #     return render action: 'waitlist'
+    #   elsif params[:waitlist] == 'all'
+    #     @reservation.waitlist_amount = @reservation.amount
+    #     @reservation.amount = 0
+    #   else
+    #     @reservation.waitlist_amount = @reservation.amount - @event.available_tickets
+    #     @reservation.amount = @event.available_tickets
+    #   end
+    # end
 
     @reservation.owner = @contact
 
@@ -79,7 +79,7 @@ class TicketReservationsController < ApplicationController
   end
 
   def get_layout
-    if ['new', 'show', 'cancel'].include? params[:action]
+    if ['new', 'create', 'show', 'cancel'].include? action_name
       'pages'
     else
       'group'
