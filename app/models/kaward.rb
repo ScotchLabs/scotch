6a56aac7821bbaa6a4d4b48ea516b0dd1bd1c15d
hmind <hmind2005@gmail.com>
2013-05-05 11:24:16 -0400

    Update kudos voting stage

--- app/models/kaward.rb
+++ app/models/kaward.rb
@@ -4,4 +4,8 @@ class Kaward < ActiveRecord::Base
   has_many :kvotes, :through => :knominations
   
   has_and_belongs_to_many :voters, :class_name => 'User', :join_table => 'voters', :uniq => true
+
+  def voting_knominations
+    self.knominations.joins(:kvotes).where('kvotes.stage IS NULL AND kvotes.positive = TRUE').group('knominations.id').order('COUNT(kvotes.id) DESC').limit(3)
+  end
 end
