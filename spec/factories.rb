448f711ed73b732b24e008a20df06c59d5e57395
Carlos Diaz-Padron <carlos@carlosdp.io>
2013-09-18 16:25:44 -0400

    Add factory spec and User factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,20 @@
 FactoryGirl.define do
+  factory :user do
+    first_name "Stephen"
+    last_name "Schwartz"
+    andrewid "stephen"
+    password "schwartz"
+    password_confirmation "schwartz"
+    home_college "CFA"
+    graduation_year Date.today.year + 4
+    smc "1337"
+    gender "Male"
+    residence "Morewood E"
+    birthday Date.new(1948, 3, 6)
+    majors "Drama"
+    minors "Musical Composition"
+    other_activities "Broadway"
+    about "I am awesome"
+    tech_skills "Being awesome"
+  end
 end
