4d36497c34db4a9760b7f6ea6c0b02ac8e464163
Carlos Diaz-Padron <carlos@carlosdp.io>
2013-09-18 17:32:07 -0400

    Add TicketReservation factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -33,7 +33,7 @@ FactoryGirl.define do
     factory :show do
       name "New Show"
       type "Show"
-      short_name "show"
+      sequence(:short_name) {|n| "show#{n}"}
       slot "Test Slot"
       price_with_id 5
       price_without_id 10
@@ -41,6 +41,12 @@ FactoryGirl.define do
       tech_start Date.today + 5.days
       tech_end Date.today + 10.days
       writers "Stephen Schwartz"
+
+      trait :tickets_available do
+        tickets_available true
+      end
+
+      factory :available_show, traits: [:tickets_available]
     end
   end
 
@@ -53,5 +59,29 @@ FactoryGirl.define do
     description "Meeting of Production Staff"
     event_type "meeting"
     session "none"
+
+    factory :performance do
+      event_type 'show'
+      max_attendance 100
+
+      trait :tickets_available do
+        association :owner, factory: :available_show
+      end
+
+      factory :open_performance, traits: [:tickets_available]
+    end
+  end
+
+  factory :contact do
+    protocol "email"
+    sequence(:address) {|n| "example#{n}@example.com"}
+    temporary_name "Stephen Sondheim"
+  end
+
+  factory :ticket_reservation do
+    association :event, factory: :open_performance
+    association :owner, factory: :contact
+    amount 1
+    with_id true
   end
 end
