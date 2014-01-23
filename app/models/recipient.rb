081465840ffde379f451a8b8069ad39974a6f527
Carlos Diaz-Padron <carlos@carlosdp.io>
2014-01-23 15:05:30 -0500

    Add to and subject methods to Recipient

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -3,6 +3,47 @@ class Recipient < ActiveRecord::Base
   belongs_to :group
   belongs_to :target, polymorphic: true
 
+  def to
+    if target.is_a? User
+      target.email
+    else
+      if group
+        "#{group.short_name}@snstheatre.org"
+        return "#{group.short_name}+#{target.short_name}@snstheatre.org" if target
+        return "#{group.short_name}+#{target_identifier.downcase.gsub(' ', '')}@snstheatre.org" if target_identifier
+      else
+        return "#{target.short_name}@snstheatre.org" if target
+        return "#{target_identifier.downcase.gsub(' ', '')}@snstheatre.org" if target_identifier
+      end
+    end
+  end
+
+  def subject
+    prefix = '['
+
+    if target.is_a? User
+      prefix += 'Scotch'
+    else
+      if group
+        prefix += group.short_name.capitalize + ' '
+
+        if target
+          prefix += target.name
+        elsif target_identifier
+          prefix += target_identifier
+        end
+      else
+        if target
+          prefix += target.name
+        elsif target_identifier
+          prefix += target_identifier
+        end
+      end
+    end
+
+    prefix + '] ' + owner.subject
+  end
+
   def envelope_recipients
     if target_identifier
       if group
