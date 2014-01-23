f03f23ebcc948d96fc45924f1f50f1e3e669c5c8
Carlos Diaz-Padron <carlos@carlosdp.io>
2014-01-23 14:02:38 -0500

    Add recipient resolution and tests

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,5 +1,29 @@
 class Recipient < ActiveRecord::Base
-  belongs_to :message_list
-  belongs_to :message
-  belongs_to :user
+  belongs_to :owner, polymorphic: true
+  belongs_to :group
+  belongs_to :target, polymorphic: true
+
+  def envelope_recipients
+    if target_identifier
+      if group
+        Position.where(group_id: group.id, display_name: target_identifier)
+        .includes(:user).map { |p| p.user.email }
+      else
+        Position.where(group_id: Group.active, display_name: target_identifier)
+        .includes(:user).map { |p| p.user.email }
+      end
+    elsif target.is_a? User
+      [target.email]
+    elsif target.is_a? Group
+      target.members.pluck(:email)
+    elsif target.is_a? Role
+      if group
+        Position.where(group_id: group, role_id: target)
+        .includes(:user).map { |p| p.user.email }
+      else
+        Position.where(group_id: Group.active, role_id: target)
+        .includes(:user).map { |p| p.user.email }
+      end
+    end
+  end
 end
