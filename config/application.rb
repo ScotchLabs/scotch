e44b2e13f1ad524098fa52a31d6d3096f3450221
Carlos Diaz-Padron <carlos@carlosdp.io>
2014-01-24 21:26:45 -0500

    Attempt to fix precompile time by disabling compression

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,7 @@ module Scotch
     config.assets.version = '1.0'
     
     config.assets.initialize_on_precompile = false
+    config.assets.compress = false
 
     # Devise layout config
     config.to_prepare do
